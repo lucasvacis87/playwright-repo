e7ec13d215a96ec1d5777e4cba61b68dab7132a7
Lucas Vacis <lucas.vacis@gmail.com>
2026-01-29 16:52:04 -0300

    more ci setup parallel

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,9 +21,11 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI
+    ? [['blob']]
+    : [['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -31,6 +33,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
     baseURL: testConfig.baseUrl,
     actionTimeout: testConfig.timeouts.action,
     navigationTimeout: testConfig.timeouts.navigation
